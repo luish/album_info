2ad64a401a54695d30aac9a2aef70b71154339c3
Luis Henrique <lshsousa@gmail.com>
2014-02-27 19:30:31 -0300

    Basic REST with Tornado

---
+++ server.py
@@ -0,0 +1,29 @@
+import tornado.web
+import tornado.ioloop
+
+from artist_info import ArtistInfo
+
+
+class ArtistSearchHandler(tornado.web.RequestHandler):
+
+    def get(self, artist_name):
+        response = ArtistInfo().search_artist(artist_name)
+        self.write(response)
+
+
+class ArtistInfoByMbidHandler(tornado.web.RequestHandler):
+
+    def get(self, mbid):
+        response = ArtistInfo().get_info(mbid)
+        self.write(response)
+
+
+app = tornado.web.Application([
+    (r'/search/([a-z0-9 -]+)', ArtistSearchHandler),
+    (r'/info/(.*)', ArtistInfoByMbidHandler),
+])
+
+if __name__ == '__main__':
+    print 'Listening on http://127.0.0.1:8888'
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
