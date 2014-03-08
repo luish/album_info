6d6ced5df461b37c2b255597f17ada4debc6a078
Luis Henrique <lshsousa@gmail.com>
2014-03-08 18:33:27 -0300

    Vagalume get artist info

--- artist_info.py
+++ artist_info.py
@@ -21,7 +21,7 @@ class ArtistInfo:
     def get_info(self, mbid, name=None):
         self.result['musicbrainz'] = MusicBrainz.get_artist_info(mbid)
         self.result['lastfm'] = LastFm().get_artist_info(mbid=mbid)
-        #self.result['vagalume'] = Vagalume().get_artist_info(mbid)
+        self.result['vagalume'] = Vagalume().get_artist_info(name)
         #self.result['discogs'] = Discogs().get_artist_info(mbid)
         self.result['echonest'] = EchoNest().get_artist_info(name)
         #self.result['coverartarchive'] = CoverArtArchive().get_artist_images(mbid)
--- lib/vagalume.py
+++ lib/vagalume.py
@@ -1,2 +1,36 @@
+from utils import *
+
+
 class Vagalume:
-    pass
+
+    def __init__(self):
+        self.base_url = 'http://www.vagalume.com.br/'
+
+
+    def get_artist_info(self, name):
+        
+        url = self.base_url + name + '/index.js'
+        json = get_json(url)
+        result = None
+
+        if 'artist' in json:
+            artist = json['artist']
+            albums = artist['albums']['item']
+
+            if 'genre' in artist:
+                genres = artist['genre']
+            else:
+                genres = None
+
+            result = {
+                'name': artist['desc'],
+                'images': {
+                    'medium': self.base_url + artist['pic_medium'], 
+                    'small' : self.base_url + artist['pic_small']
+                },
+                'vagalume_url': self.base_url + artist['url'],
+                'albums': albums,
+                'genres': genres
+            }
+
+        return result
