3a30f07b6a57a976010d1669421b938e91755912
Luis Henrique Borges <lshsousa@gmail.com>
2014-07-04 20:30:34 -0300

    base of coverartarchive - get release cover

--- lib/coverartarchive.py
+++ lib/coverartarchive.py
@@ -1,2 +1,26 @@
+from utils import *
+
+
+BASE_URL = 'http://coverartarchive.org/'
+
+
 class CoverArtArchive:
-    pass
+    
+    def get_release_cover(self, release_id):
+
+    	url = BASE_URL + 'release/' + release_id
+
+    	try:
+    		json = get_json(url)
+    		return json
+
+    	except Exception, e:
+    		pass
+
+    	return None
+
+
+## test
+
+#cover_art = CoverArtArchive()
+#print cover_art.get_release_cover('76df3287-6cda-33eb-8e9a-044b5e15ffdd')
